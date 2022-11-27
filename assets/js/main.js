1aa020e6549de12499724c80666c0b33bc56d5ac
eduardoslompo <edu_slompo@hotmail.com>
2022-11-27 00:12:15 -0300

    3 files modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
 function capture() {
+  for (let i = 0; i < 7; i++) {
+    //Substituir a textarea por uma variavel com o seu conteúdo
+    let x = document.getElementById('areatexto').value;
+    document.getElementById('texto').innerHTML = x;
+
+    //Trocar background do texto
+    document.getElementById('texto').id = 'texto2';
+    //Remover a textarea do DOM
+    let b = document.getElementById('areatexto');
+    b.parentNode.removeChild(b);
+
+    //container 2
+    let y = document.getElementById('areatexto2').value;
+    document.getElementById('texto3').innerHTML = y;
+
+    document.getElementById('texto3').id = 'texto4';
+
+    const c = document.getElementById('areatexto2');
+    c.parentNode.removeChild(c);
+  }
+
+  document.getElementById('botoes').style.display = 'none';
+
+
   html2canvas(document.body, {
     onrendered: function (canvas) {
       var image = canvas
@@ -52,6 +76,3 @@ function callFunctions() {
   sumirBotoes();
   capture();
 }
-
-let run = document.getElementById('rodar');
-run.addEventListener('click', funcaoTexto());
