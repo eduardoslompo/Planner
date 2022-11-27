df994256344b04d498c5bd9e0b26471e7a649320
eduardoslompo <edu_slompo@hotmail.com>
2022-11-27 00:58:22 -0300

    1 file modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,14 +18,14 @@ function capture() {
 }
 
 function funcaoTexto() {
-  let x = document.getElementById('areatexto').value;
-  let y = document.getElementById('areatexto2').value;
-
   for (let i = 0; i < 7; i++) {
     //Substituir a textarea por uma variavel com o seu conteúdo
-    //let x = document.getElementById('areatexto').value;
-    document.getElementById('texto').textContent = x;
-
+    if(document.getElementById('areatexto').value === ''){
+      document.getElementById('texto').innerHTML = '';
+    } else{
+      let x = document.getElementById('areatexto').value;
+      document.getElementById('texto').innerHTML = x;
+    }
     //Trocar background do texto
     document.getElementById('texto').id = 'texto2';
     //Remover a textarea do DOM
@@ -33,9 +33,12 @@ function funcaoTexto() {
     b.parentNode.removeChild(b);
 
     //container 2
-    //let y = document.getElementById('areatexto2').value;
-    document.getElementById('texto3').textContent = y;
-
+    if(document.getElementById('areatexto2').value === ''){
+      document.getElementById('areatexto2').value = '';
+    } else{
+      let y = document.getElementById('areatexto2').value;
+      document.getElementById('texto3').innerHTML = y;
+    }
     document.getElementById('texto3').id = 'texto4';
 
     const c = document.getElementById('areatexto2');
